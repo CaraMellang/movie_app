8509cbb530096674fea4122183489008edaeb548
Mellang <dlwpgh23@naver.com>
2021-07-29 23:57:02 +0900

    JSX와 Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const Food = ({ fav }) => {
+  return <span>{fav}이 달아..</span>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Food</h1>
+      <Food fav="caramel" />
     </div>
   );
 }
