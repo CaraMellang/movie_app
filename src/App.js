1467a387c9057428afc95380f6a0a1a5dfbf81f6
Mellang <dlwpgh23@naver.com>
2021-07-31 23:40:42 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import "./App.css";
 
-const Food = ({ fav }) => {
-  return <span>{fav}이 달아..</span>;
+const Food = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h2>
+        {props.name}이 {props.text}{" "}
+      </h2>
+    </div>
+  );
 };
+
+const fufufu = [
+  {
+    id: 1,
+    name: "멜랑",
+    text: "똑똑함",
+  },
+  {
+    id: 2,
+    name: "중마",
+    text: "박식함",
+  },
+  {
+    id: 3,
+    name: "빵국",
+    text: "로아함",
+  },
+];
 
 function App() {
   return (
     <div>
       <h1>Food</h1>
-      <Food fav="caramel" />
+      {fufufu.map((hihi) => {
+        return <Food key={hihi.id} name={hihi.name} text={hihi.text} />;
+      })}
     </div>
   );
 }
